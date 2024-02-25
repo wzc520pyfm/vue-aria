e1de5704c79223707df24accab022353237c2a34
wzc520pyfm <1528857653@qq.com>
2024-02-25 16:15:31 +0800

    feat(storybook): :sparkles: add storybook basic project and config

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,24 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    "../../components/**/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  staticDirs: ["../public"],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
---
+++ packages/storybook/.storybook/preview.ts
@@ -0,0 +1,29 @@
+/** @type { import('@storybook/vue3').Preview } */
+import {setup, Preview} from '@storybook/vue3'
+import './style.css'
+
+setup((app) => {})
+
+const decorators: Preview["decorators"] = [
+  (story) => ({
+    components: {story},
+    template: '<story />',
+  }),
+]
+
+const parameters: Preview["parameters"] = {
+  actions: {argTypesRegex: '^on[A-Z].*'},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
+    },
+  },
+}
+
+const preview: Preview = {
+  decorators,
+  parameters,
+}
+
+export default preview
