775a84fd9042cfc3160d6d76e229a2804f3bbd17
wzc520pyfm <1528857653@qq.com>
2024-03-03 23:44:18 +0800

    build(preset): [button] add shortcuts and shorthands for unocss

--- packages/@preset/button/src/index.ts
+++ packages/@preset/button/src/index.ts
@@ -9,6 +9,16 @@ export const presetNevUIButton = definePreset((options: PresetNevUISystemOptions
     ...presetNevUISystem(options),
     name: '@nev-ui/preset-button',
     // and other custom configurations
+    shortcuts: [
+      {
+        btn: 'bg-blue-400 text-white px-20px py-12px text-14px cursor-pointer inline-block border-0 rounded-3em line-height-none',
+      },
+    ],
+    autocomplete: {
+      shorthands: {
+        btn: 'btn',
+      },
+    },
   }
 })
 
