dc144ffed0ad29c5595b808a3329764b49b80b6c
wzc520pyfm <1528857653@qq.com>
2024-03-04 23:44:12 +0800

    refactor(playground): :recycle: use shared func

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,13 +1,10 @@
 import { resolve } from 'node:path'
-import fs from 'fs-extra'
+import { getSubDirNames } from '@nev-ui/shared'
 import { defineConfig } from 'unocss'
 import { presetNevUI } from '@nev-ui/preset'
 
 const presetDir = resolve(__dirname, '../packages/@preset')
-const presetNames = fs
-  .readdirSync(presetDir, { withFileTypes: true })
-  .filter((d) => d.isDirectory())
-  .map((d) => d.name)
+const presetNames = getSubDirNames(presetDir)
 
 export default defineConfig({
   presets: [
