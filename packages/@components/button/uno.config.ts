7297fc9abd623d0460613caf8c4346551a6ccd8b
wzc520pyfm <1528857653@qq.com>
2024-03-04 22:59:00 +0800

    refactor(dev): :recycle: remove magic string in unocss config

--- packages/@components/button/uno.config.ts
+++ packages/@components/button/uno.config.ts
@@ -1,7 +1,15 @@
+import {resolve} from 'node:path'
+import fs from 'fs-extra'
 import {defineConfig} from 'unocss'
 import {presetNevUIButton} from '@nev-ui/preset-button'
 import type {UserConfig} from 'unocss'
 import type {Theme} from '@nev-ui/preset-button'
+
+const presetDir = resolve(__dirname, '../../@preset')
+const presetNames = fs
+  .readdirSync(presetDir, {withFileTypes: true})
+  .filter((d) => d.isDirectory())
+  .map((d) => d.name)
 
 export default defineConfig<Theme>({
   presets: [
@@ -11,7 +19,6 @@ export default defineConfig<Theme>({
   ],
   configDeps: [
     '../../preset/dist/index.js',
-    '../../@preset/system/dist/index.js',
-    '../../@preset/button/dist/index.js',
+    ...presetNames.map((name) => `../../@preset/${name}/dist/index.js`),
   ],
 }) as UserConfig<Theme>
--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,15 +1,22 @@
-import {defineConfig} from 'unocss'
-import {presetNevUI} from '@nev-ui/preset'
+import { resolve } from 'node:path'
+import fs from 'fs-extra'
+import { defineConfig } from 'unocss'
+import { presetNevUI } from '@nev-ui/preset'
+
+const presetDir = resolve(__dirname, '../packages/@preset')
+const presetNames = fs
+  .readdirSync(presetDir, { withFileTypes: true })
+  .filter((d) => d.isDirectory())
+  .map((d) => d.name)
 
 export default defineConfig({
   presets: [
     presetNevUI({
       /* preset options */
-    }),
+    })
   ],
   configDeps: [
     '../packages/preset/dist/index.js',
-    '../packages/@preset/system/dist/index.js',
-    '../packages/@preset/button/dist/index.js',
+    ...presetNames.map((name) => `../packages/@preset/${name}/dist/index.js`)
   ]
 })
