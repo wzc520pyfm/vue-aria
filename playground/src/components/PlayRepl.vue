8619d20fc3063f5a02b88e5c48b2bd1a48390f61
wzc520pyfm <1528857653@qq.com>
2024-03-18 22:32:28 +0800

    chore(playground): :hammer: add reactive test

--- playground/src/components/PlayRepl.vue
+++ playground/src/components/PlayRepl.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-import { Button } from '@nev-ui/vue'
+import { Button, type ButtonProps } from '@nev-ui/vue'
+import { ref } from 'vue'
 
 defineOptions({ name: 'PlayRepl' })
+
+const as = ref<ButtonProps['as']>('button')
+const size = ref<ButtonProps['size']>('sm')
+const color = ref<ButtonProps['color']>('primary')
+const radius = ref<ButtonProps['radius']>('none')
+
+const changeSize = () => {
+  console.log('changeSize', size.value)
+  as.value = as.value === 'button' ? 'a' : 'button'
+  size.value = size.value === 'sm' ? 'lg' : 'sm'
+  color.value = color.value === 'primary' ? 'default' : 'primary'
+  radius.value = radius.value === 'none' ? 'full' : 'none'
+}
 </script>
 
 <template>
-  <Button>Button</Button>
-  <button class="btn btn-primary">Button2</button>
+  <Button :as="as" :size="size" :color="color" :radius="radius">Button</Button>
+  <button class="btn btn-primary" @click="changeSize">Button2</button>
 </template>
 
 <style scoped></style>
