f5d64fc8793a1e1298d1c1232961ac9bd0b952b2
wzc520pyfm <1528857653@qq.com>
2024-05-23 23:59:55 +0800

    feat(preset): :sparkles: [button] add hover, disable and full

--- packages/@preset/button/src/shortcuts.ts
+++ packages/@preset/button/src/shortcuts.ts
@@ -5,8 +5,9 @@ export const buttonShortcuts: Record<string, string> = {
   'btn-sm': 'px-3 min-w-16 h-8 text-xs gap-2 rounded-sm',
   'btn-md': 'px-4 min-w-20 h-10 text-sm gap-2 rounded-md',
   'btn-lg': 'px-6 min-w-24 h-12 text-base gap-3 rounded-lg',
-  'btn-default': 'bg-#d4d4d8 text-#11181C', // color should has a system
-  'btn-primary': 'bg-#006FEE text-#FFFFFF',
+  // color and hover should has a system
+  'btn-default': 'bg-#d4d4d8 text-#11181C data-[hover=true]:bg-#d4d4d8/40',
+  'btn-primary': 'bg-#006FEE text-#FFFFFF data-[hover=true]:bg-#006FEE/20',
   // more colors
   // ...
   'btn-rounded-none': 'rounded-none',
@@ -14,6 +15,9 @@ export const buttonShortcuts: Record<string, string> = {
   'btn-rounded-md': 'rounded-md',
   'btn-rounded-lg': 'rounded-lg',
   'btn-rounded-full': 'rounded-full',
+  'btn-full': 'w-full',
+  // should extends opacity-disabled from preset system to instead opacity-50
+  'btn-disabled': 'opacity-50 pointer-events-none',
 }
 
 export function getButtonSafeList() {
