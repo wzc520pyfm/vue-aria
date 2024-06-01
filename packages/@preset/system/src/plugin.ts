be1192e60401c2be2dbab8d48d150c1c99c1e991
wzc520pyfm <1528857653@qq.com>
2024-06-01 20:10:56 +0800

    feat(preset): :sparkles: [system] add custom theme plugin

---
+++ packages/@preset/system/src/plugin.ts
@@ -0,0 +1,56 @@
+import {get, omit} from 'radash'
+import forEach from 'lodash.foreach'
+import deepMerge from 'deepmerge'
+import {semanticColors} from './colors'
+import {isBaseTheme} from './utils/theme'
+import type {Theme as PresetMiniTheme} from '@unocss/preset-mini'
+
+export interface Theme extends PresetMiniTheme {
+  extend?: string
+}
+
+interface NevUIPlugin {
+  // userTheme
+  theme?: Record<string, Theme>
+  defaultExtendTheme?: 'light' | 'dark'
+}
+
+export const nevui = (config: NevUIPlugin = {}) => {
+  const {theme: themeObject = {}, defaultExtendTheme = 'light'} = config
+
+  const userLightColors = get(themeObject, 'light.colors', {})
+  const userDarkColors = get(themeObject, 'dark.colors', {})
+  // TODO: merge more default config
+
+  // get other theme from the config different from light and dark
+  const otherTheme = omit(themeObject, ['light', 'dark']) || {}
+
+  forEach(otherTheme, ({extend, colors}, themeName) => {
+    const baseTheme = extend && isBaseTheme(extend) ? extend : defaultExtendTheme
+
+    // TODO: just merge the colors for now
+    if (colors && typeof colors === 'object') {
+      otherTheme[themeName].colors = deepMerge(semanticColors[baseTheme], colors)
+    }
+  })
+
+  const light: Theme = {
+    ...get(themeObject, 'light', {}),
+    colors: deepMerge(semanticColors.light, userLightColors),
+  }
+
+  const dark: Theme = {
+    ...get(themeObject, 'dark', {}),
+    colors: deepMerge(semanticColors.dark, userDarkColors),
+  }
+
+  const theme = {
+    light,
+    dark,
+    ...otherTheme,
+  }
+
+  return {
+    theme,
+  }
+}
