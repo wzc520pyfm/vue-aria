4304a1be83dcd7b39eae366b19dc6092c852285a
wzc520pyfm <1528857653@qq.com>
2024-06-01 20:12:36 +0800

    feat(preset): :sparkles: [button] deep merge system preset

--- packages/@preset/button/src/preset.ts
+++ packages/@preset/button/src/preset.ts
@@ -1,19 +1,21 @@
 import {definePreset} from '@unocss/core'
 import {presetNevUISystem} from '@nev-ui/preset-system'
+import deepMerge from 'deepmerge'
 import {buttonShortcuts, getButtonSafeList} from './shortcuts'
 import type {PresetNevUISystemOptions, Theme} from '@nev-ui/preset-system'
 
 export type {Theme}
 
 export const presetNevUIButton = definePreset((options: PresetNevUISystemOptions = {}) => {
-  return {
-    ...presetNevUISystem(options),
+  const preset = {
     name: '@nev-ui/preset-button',
     // and other custom configurations
     // ...
     shortcuts: buttonShortcuts,
     safelist: getButtonSafeList(),
   }
+
+  return deepMerge(presetNevUISystem(options), preset)
 })
 
 export default presetNevUIButton
