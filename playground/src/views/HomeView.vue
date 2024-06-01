decf12a2db234a32f8664dc5dac4e843f1a447b0
wzc520pyfm <1528857653@qq.com>
2024-06-01 17:03:38 +0800

    feat(playground): :sparkles: add toggle dark

--- playground/src/views/HomeView.vue
+++ playground/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
 import PlayRepl from '@/components/PlayRepl.vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
   <main>
+    <div
+      class="fixed top-4 right-4 z-50 p-2 text-2xl cursor-pointer"
+      @click="() => toggleDark()"
+    >{{ isDark ? '🌞' : '🌛' }}</div>
     <PlayRepl />
   </main>
 </template>
