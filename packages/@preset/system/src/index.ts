8f5cd4054eac3c6ebb0f577dd41a1a782540767f
wzc520pyfm <1528857653@qq.com>
2024-06-01 20:11:57 +0800

    feat(preset): :sparkles: [system] support custom theme

--- packages/@preset/system/src/index.ts
+++ packages/@preset/system/src/index.ts
@@ -1,17 +1,33 @@
 import {definePreset} from '@unocss/core'
 import {presetMini} from '@unocss/preset-mini'
-import type {PresetMiniOptions, Theme} from '@unocss/preset-mini'
+import presetTheme from 'unocss-preset-theme'
+import {all as deepMergeAll} from 'deepmerge'
+import {get, omit} from 'radash'
+import {nevui} from './plugin'
+import type {Theme} from './plugin'
+import type {PresetMiniOptions} from '@unocss/preset-mini'
 
 export type {Theme}
 
-export interface PresetNevUISystemOptions extends PresetMiniOptions {}
+export interface PresetNevUISystemOptions extends PresetMiniOptions {
+  theme?: Record<string, Theme>
+}
 
 export const presetNevUISystem = definePreset((options: PresetNevUISystemOptions = {}) => {
-  return {
-    ...presetMini(options),
+  const {theme} = nevui(options)
+  const lightTheme = get(theme, 'light', {})
+  const otherTheme = omit(theme, ['light']) || {}
+  const preset = {
     name: '@nev-ui/preset-system',
+    theme: lightTheme,
     // and other custom configurations
   }
+
+  return deepMergeAll([
+    presetMini(options),
+    presetTheme<Theme>({prefix: '--nevui', theme: otherTheme}),
+    preset,
+  ])
 })
 
 export default presetNevUISystem
