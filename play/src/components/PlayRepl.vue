2307ebd6436749e12566f698d7997c2db86afb13
wzc520pyfm <1528857653@qq.com>
2024-06-05 00:11:12 +0800

    chore(playground): :hammer: add more test button example

--- play/src/components/PlayRepl.vue
+++ play/src/components/PlayRepl.vue
@@ -7,22 +7,36 @@ defineOptions({ name: 'PlayRepl' })
 const as = ref<ButtonProps['as']>('button')
 const size = ref<ButtonProps['size']>('sm')
 const color = ref<ButtonProps['color']>('primary')
-const radius = ref<ButtonProps['radius']>('none')
+const radius = ref<ButtonProps['radius']>('lg')
 
 const changeSize = () => {
   console.log('changeSize', size.value)
   as.value = as.value === 'button' ? 'a' : 'button'
-  size.value = size.value === 'sm' ? 'lg' : 'sm'
+  size.value = size.value === 'sm' ? 'md' : 'sm'
   color.value = color.value === 'primary' ? 'default' : 'primary'
-  radius.value = radius.value === 'none' ? 'full' : 'none'
+  radius.value = radius.value === 'lg' ? 'full' : 'lg'
 }
 </script>
 
 <template>
+  <Button :as="as" :size="size" color="default" :radius="radius">Button</Button>
   <Button :as="as" :size="size" :color="color" :radius="radius" isDisabled>Button</Button>
   <Button :as="as" :size="size" color="secondary" :radius="radius">Button</Button>
+  <Button :as="as" :size="size" color="success" :radius="radius">Button</Button>
+  <Button :as="as" :size="size" color="warning" :radius="radius">Button</Button>
+  <Button :as="as" :size="size" color="danger" :radius="radius">Button</Button>
   <Button :as="as" :size="size" :color="color" :radius="radius" fullWidth>Button</Button>
   <button class="btn btn-primary" @click="changeSize">Button2</button>
+  <Button :as="as" size="sm" :color="color" :radius="radius">Button</Button>
+  <Button :as="as" size="md" :color="color" :radius="radius">Button</Button>
+  <Button :as="as" size="lg" :color="color" :radius="radius">Button</Button>
+  <div>
+    <Button :as="as" :size="size" :color="color" radius="none">Button</Button>
+    <Button :as="as" :size="size" :color="color" radius="sm">Button</Button>
+    <Button :as="as" :size="size" :color="color" radius="md">Button</Button>
+    <Button :as="as" :size="size" :color="color" radius="lg">Button</Button>
+    <Button :as="as" :size="size" :color="color" radius="full">Button</Button>
+  </div>
 </template>
 
 <style scoped></style>
