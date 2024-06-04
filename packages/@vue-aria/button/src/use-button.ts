b74e38edef61094daa062486b380201119ca22fe
wzc520pyfm <1528857653@qq.com>
2024-06-04 23:09:15 +0800

    refactor(vue-aria): :recycle: 【button] use to-value to implement use-btn

--- packages/@vue-aria/button/src/use-button.ts
+++ packages/@vue-aria/button/src/use-button.ts
@@ -2,7 +2,7 @@ import {computed, toValue} from 'vue'
 import {useHover} from '@nev-ui/use-hover'
 import {usePress} from '@nev-ui/use-press'
 import {dataAttr, mergeProps} from '@nev-ui/shared'
-import {withButtonDefault} from './with-button-default'
+import type {MaybeRefOrGetter} from 'vue'
 import type {AriaButtonProps} from '@nev-ui/types-aria-button'
 
 export interface UseButtonProps extends AriaButtonProps {
@@ -15,31 +15,43 @@ export interface UseButtonEmits {
   (e: 'click'): void
 }
 
+const BUTTON_DEFAULT = {
+  as: 'button',
+  type: 'button',
+} as const
+
+// TODO: move to type shared
+type PropMaybeRefOrGetter<T> = {
+  [P in keyof T]?: MaybeRefOrGetter<T[P]>
+}
+
 /**
  * Hook for headless button
  * @param props - Button props
  * @param emits - Button emits
  */
-export function useButton(props: UseButtonProps, emits: UseButtonEmits) {
-  const {Component, type, isDisabled} = withButtonDefault(props)
+export function useButton(props: PropMaybeRefOrGetter<UseButtonProps> = {}, emits: UseButtonEmits) {
+  const {as = BUTTON_DEFAULT.as, type = BUTTON_DEFAULT.type, isDisabled = false} = props
 
-  const {isHovered, hoverProps} = useHover({isDisabled: isDisabled.value})
-  const {isPressed, pressProps} = usePress({isDisabled: isDisabled.value})
+  const Component = computed(() => toValue(as))
+
+  const {isHovered, hoverProps} = useHover({isDisabled: toValue(isDisabled)})
+  const {isPressed, pressProps} = usePress({isDisabled: toValue(isDisabled)})
   const additionalProps = computed(() =>
     Component.value === 'button'
       ? {
-          type: type.value,
-          disabled: isDisabled.value,
+          type: toValue(type),
+          disabled: toValue(isDisabled),
         }
       : {
           role: 'button',
-          tabIndex: isDisabled ? undefined : 0,
-          // href: Component.value === 'a' && isDisabled ? undefined : href,
+          tabIndex: toValue(isDisabled) ? undefined : 0,
+          // href: Component.value === 'a' && toValue(isDisabled) ? undefined : href,
           // target: Component.value === 'a' ? target : undefined,
-          type: Component.value === 'input' ? type.value : undefined,
-          disabled: Component.value === 'input' ? isDisabled.value : undefined,
+          type: Component.value === 'input' ? toValue(type) : undefined,
+          disabled: Component.value === 'input' ? toValue(isDisabled) : undefined,
           'aria-disabled':
-            !isDisabled || Component.value === 'input' ? undefined : isDisabled.value,
+            !toValue(isDisabled) || Component.value === 'input' ? undefined : toValue(isDisabled),
           // rel: Component.value === 'a' ? rel : undefined,
         },
   )
@@ -59,7 +71,7 @@ export function useButton(props: UseButtonProps, emits: UseButtonEmits) {
 
   return {
     Component,
-    isDisabled,
+    isDisabled: computed(() => toValue(isDisabled)),
     getButtonProps,
     getButtonEvents,
   }
