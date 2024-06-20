93b5d02f3976c4fb4c53c280e471f75c3256c246
wzc520pyfm <1528857653@qq.com>
2024-06-21 00:23:35 +0800

    chore(utilities): :hammer: update comment

--- packages/@utilities/shared/src/mergeProps.ts
+++ packages/@utilities/shared/src/mergeProps.ts
@@ -4,6 +4,7 @@ interface Props {
 
 type PropsArg = Props | null | undefined
 
+// TODO: improve typing
 export function mergeProps<T extends PropsArg[]>(...args: T) {
   // Start with a base clone of the first argument. This is a lot faster than starting
   // with an empty object and adding properties as we go.
