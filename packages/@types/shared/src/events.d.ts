e37b4975bdaab16589abd7da64bc10f2fd44afa9
wzc520pyfm <1528857653@qq.com>
2024-06-20 23:31:21 +0800

    feat(types): :sparkles: [events] add event-handlers type

--- packages/@types/shared/src/events.d.ts
+++ packages/@types/shared/src/events.d.ts
@@ -1,5 +1,9 @@
 export type PointerType = 'mouse' | 'pen' | 'touch' | 'keyboard' | 'virtual'
 
+export type EventHandlers<E> = {
+  [K in keyof E]?: E[K] extends (...args: any) => any ? E[K] : (payload: E[K]) => void
+}
+
 export interface PressEvent {
   /** The type of press event being fired. */
   type: 'pressstart' | 'pressend' | 'pressup' | 'press'
