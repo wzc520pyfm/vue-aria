444ab59ccb20ba7fdc3a2ea110def580240ba6ee
wzc520pyfm <1528857653@qq.com>
2024-06-20 23:12:52 +0800

    fix(preset): :bug: 【button] add preset type

--- packages/@preset/button/src/preset.ts
+++ packages/@preset/button/src/preset.ts
@@ -2,20 +2,23 @@ import {definePreset} from '@unocss/core'
 import {presetNevUISystem} from '@nev-ui/preset-system'
 import deepMerge from 'deepmerge'
 import {buttonShortcuts, getButtonSafeList} from './shortcuts'
+import type {PresetFactory} from '@unocss/core'
 import type {Layout, PresetNevUISystemOptions, Theme, Themes} from '@nev-ui/preset-system'
 
 export type {Theme, Themes, Layout}
 
-export const presetNevUIButton = definePreset((options: PresetNevUISystemOptions = {}) => {
-  const preset = {
-    name: '@nev-ui/preset-button',
-    // and other custom configurations
-    // ...
-    shortcuts: buttonShortcuts,
-    safelist: getButtonSafeList(),
-  }
+export const presetNevUIButton: PresetFactory<Theme, PresetNevUISystemOptions> = definePreset(
+  (options: PresetNevUISystemOptions = {}) => {
+    const preset = {
+      name: '@nev-ui/preset-button',
+      // and other custom configurations
+      // ...
+      shortcuts: buttonShortcuts,
+      safelist: getButtonSafeList(),
+    }
 
-  return deepMerge(presetNevUISystem(options), preset)
-})
+    return deepMerge(presetNevUISystem(options), preset)
+  },
+)
 
 export default presetNevUIButton
