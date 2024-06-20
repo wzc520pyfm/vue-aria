b18a7cf42283e755c96f385a40ce12b4508f50b6
wzc520pyfm <1528857653@qq.com>
2024-06-20 23:57:55 +0800

    chore(components): :hammer: 【button] update comment

--- packages/@vue-aria-components/button/src/button.ts
+++ packages/@vue-aria-components/button/src/button.ts
@@ -1,7 +1,7 @@
 import type {UseButtonEmits, UseButtonProps} from '@nev-ui/use-aria-button'
 
 // Vue build error when interface implementation is empty.
-// The Vue solution is to use `@vue-ignore` comment.
+// The Vue official solution is to use `@vue-ignore` comment.
 export interface ButtonProps extends /* @vue-ignore */ UseButtonProps {}
 
 export interface ButtonEmits extends UseButtonEmits {}
