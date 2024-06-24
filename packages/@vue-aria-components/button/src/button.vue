afcac6a6dc9f35520ab219bbf37c1461e2ea4942
wzc520pyfm <1528857653@qq.com>
2024-06-24 23:08:15 +0800

    fix(vue-aria-components): :bug: [button] fix reactive error

--- packages/@vue-aria-components/button/src/button.vue
+++ packages/@vue-aria-components/button/src/button.vue
@@ -2,7 +2,7 @@
   <component
     :is="Component"
     :class="[bem.b()]"
-    v-bind="{...getButtonProps()}"
+    v-bind="{...getButtonProps(), ...getButtonEvents()}"
     v-on="{...getButtonEvents()}"
   >
     <slot />
@@ -10,7 +10,9 @@
 </template>
 
 <script setup lang="ts">
-// import {toRefs} from 'vue'
+import {toRefs} from 'vue'
+import {toValuesForFunction} from '@nev-ui/utilities-vue'
+// todo: 删除v-on，事件仍可生效（包括事件修饰符），所以，是否不需要将emit和prop分开设置？
 import {useBEM} from '@nev-ui/utilities-bem'
 import {useButton} from '@nev-ui/use-aria-button'
 import type {ButtonEmits, ButtonProps} from './button'
@@ -26,7 +28,10 @@ const props = defineProps<ButtonProps>()
 const bem = useBEM('button')
 
 // Provides rendering properties and events
-const {Component, getButtonProps, getButtonEvents} = useButton(props, emits)
+const {Component, getButtonProps, getButtonEvents} = useButton(
+  toValuesForFunction(toRefs(props)),
+  emits,
+)
 </script>
 
 <style scoped></style>
