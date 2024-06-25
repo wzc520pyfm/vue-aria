8397ba34294880dea0a3873600520794d2cffa1c
wzc520pyfm <1528857653@qq.com>
2024-06-25 23:05:20 +0800

    fix(utilities): :bug: [vue] fix type error

--- packages/@utilities/vue/src/toValuesForFunction.ts
+++ packages/@utilities/vue/src/toValuesForFunction.ts
@@ -3,11 +3,12 @@ import type {Ref, ToRefs} from 'vue'
 
 // TODO: move util type for shared
 type NonUndefined<T> = T extends undefined ? never : T
-export type ToValuesForFunction<T extends ToRefs<T>> = {
+// todo: the object maybe adjust to { [key: string | number | symbol]: Ref<any> }
+export type ToValuesForFunction<T extends object> = {
   // eslint-disable-next-line @typescript-eslint/ban-types
   [K in keyof T]: T[K] extends Ref<NonUndefined<infer V>> ? (V extends Function ? V : T[K]) : never
 }
-export function toValuesForFunction<T extends ToRefs<T>>(obj: T): ToValuesForFunction<T> {
+export function toValuesForFunction<T extends object>(obj: ToRefs<T>): ToValuesForFunction<T> {
   return Object.fromEntries(
     Object.entries<Ref<unknown>>(obj).map(([key, value]) => [
       key,
