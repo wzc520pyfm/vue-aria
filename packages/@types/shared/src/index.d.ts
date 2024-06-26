db45aed0d20fc8d93e34bce878b520139532ea3a
wzc520pyfm <1528857653@qq.com>
2024-06-27 00:14:03 +0800

    feat(types): :sparkles: [shared] add utility types

--- packages/@types/shared/src/index.d.ts
+++ packages/@types/shared/src/index.d.ts
@@ -1,3 +1,4 @@
 export * from './events'
 export * from './aria'
 export * from './dom'
+export * from './utility'
--- packages/@types/shared/src/utility.d.ts
+++ packages/@types/shared/src/utility.d.ts
@@ -1,1 +1,35 @@
-/** String */
+import type {MaybeRefOrGetter} from 'vue'
+
+/** Basic */
+export type NonUndefined<T> = T extends undefined ? never : T
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I,
+) => void
+  ? I
+  : never
+
+/** Object */
+export type Duplicate<T> = {[P in keyof T]: T[P]}
+export type OmitNever<T extends object> = Duplicate<T>[keyof T]
+export type FunctionKeys<T extends object> = OmitNever<
+  Required<{
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    [K in keyof T]: NonUndefined<T[K]> extends Function ? K : never
+  }>
+>
+export type RecordToUnion<T extends Record<string, any>> = T[keyof T]
+
+/** Vue */
+export type ToMaybeRefOrGettersForNonFunction<T extends object> = {
+  [P in keyof T]?: P extends FunctionKeys<T> ? T[P] : MaybeRefOrGetter<NonUndefined<T[P]>>
+}
+type _EventsToEmits<T extends object> = UnionToIntersection<
+  RecordToUnion<{
+    [K in keyof T]: K extends `on${infer U}`
+      ? T[K] extends (...args: infer E) => infer R
+        ? (eventname: Lowercase<U>, ...args: E) => R
+        : never
+      : never
+  }>
+>
+export type EventsToEmits<T extends object> = _EventsToEmits<Required<T>>
