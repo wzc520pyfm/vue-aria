d5191ab3acd15a4b325dd9ec64f439f8647ea812
wzc520pyfm <1528857653@qq.com>
2024-06-26 20:59:37 +0800

    feat(vue-aria-components): :sparkles: remove events

--- packages/@vue-aria-components/button/src/button.vue
+++ packages/@vue-aria-components/button/src/button.vue
@@ -1,17 +1,11 @@
 <template>
-  <component
-    :is="Component"
-    :class="[bem.b()]"
-    v-bind="{...getButtonProps(), ...getButtonEvents()}"
-    v-on="{...getButtonEvents()}"
-  >
+  <component :is="Component" :class="[bem.b()]" v-bind="{...$attrs, ...getButtonProps()}">
     <slot />
   </component>
 </template>
 
 <script setup lang="ts">
 import {toRefsForNonFunction} from '@nev-ui/utilities-vue'
-// todo: 删除v-on，事件仍可生效（包括事件修饰符），所以，是否不需要将emit和prop分开设置？
 import {useBEM} from '@nev-ui/utilities-bem'
 import {useButton} from '@nev-ui/use-aria-button'
 import type {ButtonEmits, ButtonProps} from './button'
@@ -26,8 +20,8 @@ const props = defineProps<ButtonProps>()
 // Provides css layer
 const bem = useBEM('button')
 
-// Provides rendering properties and events
-const {Component, getButtonProps, getButtonEvents} = useButton(toRefsForNonFunction(props), emits)
+// Provides rendering properties
+const {Component, getButtonProps} = useButton(toRefsForNonFunction(props), emits)
 </script>
 
 <style scoped></style>
