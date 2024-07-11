7c075609ca556568590df10151d20cd0545faa7c
wzc520pyfm <1528857653@qq.com>
2024-07-12 00:15:44 +0800

    chore(types): :hammer: [button] add type for preset

--- packages/@preset/button/src/preset.ts
+++ packages/@preset/button/src/preset.ts
@@ -2,14 +2,14 @@ import {definePreset} from '@unocss/core'
 import {presetNevUISystem} from '@nev-ui/preset-system'
 import deepMerge from 'deepmerge'
 import {buttonShortcuts, getButtonSafeList} from './shortcuts'
-import type {PresetFactory} from '@unocss/core'
+import type {Preset, PresetFactory} from '@unocss/core'
 import type {Layout, PresetNevUISystemOptions, Theme, Themes} from '@nev-ui/preset-system'
 
 export type {Theme, Themes, Layout}
 
 export const presetNevUIButton: PresetFactory<Theme, PresetNevUISystemOptions> = definePreset(
   (options: PresetNevUISystemOptions = {}) => {
-    const preset = {
+    const preset: Preset<Theme> = {
       name: '@nev-ui/preset-button',
       // and other custom configurations
       // ...
