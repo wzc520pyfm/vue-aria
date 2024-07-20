837f5565015408516fde1ebea1cb54f6d81a45da
wzc520pyfm <1528857653@qq.com>
2024-07-21 01:21:36 +0800

    feat(preset): :sparkles: add icon preset

--- packages/preset/src/shortcuts.ts
+++ packages/preset/src/shortcuts.ts
@@ -1,12 +1,14 @@
 import {buttonShortcuts, getButtonSafeList} from '@nev-ui/preset-button'
 import {getSpinnerSafeList, spinnerShortcuts} from '@nev-ui/preset-spinner'
+import {getIconSafeList, iconShortcuts} from '@nev-ui/preset-icon'
 
-export const uiShortcuts = [buttonShortcuts, spinnerShortcuts]
+export const uiShortcuts = [buttonShortcuts, spinnerShortcuts, iconShortcuts]
 
 export function getUISafeList() {
   const buttonSafeList = getButtonSafeList()
   const spinnerSafeList = getSpinnerSafeList()
-  return [...buttonSafeList, ...spinnerSafeList]
+  const iconSafeList = getIconSafeList()
+  return [...buttonSafeList, ...spinnerSafeList, ...iconSafeList]
 }
 
 export {buttonShortcuts, spinnerShortcuts}
